4a452551b2144b268bfe499e1f96dafef209d78e
TaemHam <tk4003950@gmail.com>
2023-02-16 22:42:12 +0900

    Fix: "순환 참조 버그 수정"
    `JwtTokenProvider` 와 `AccountService` 가 순환 참조 되고 있어, `UserDetailsService` 가 구현되는 부분을 추출해 오류를 해결했다.

--- src/main/java/com/filmdoms/community/account/service/AccountService.java
+++ src/main/java/com/filmdoms/community/account/service/AccountService.java
@@ -1,12 +1,13 @@
 package com.filmdoms.community.account.service;
 
+import com.filmdoms.community.account.config.jwt.JwtTokenProvider;
+import com.filmdoms.community.account.data.constants.AccountRole;
 import com.filmdoms.community.account.data.dto.AccountDto;
 import com.filmdoms.community.account.data.entity.Account;
 import com.filmdoms.community.account.exception.ApplicationException;
 import com.filmdoms.community.account.exception.ErrorCode;
 import com.filmdoms.community.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,43 +17,35 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final PasswordEncoder passwordEncoder;
-
-    /**
-     * 유저 ID로 계정 정보를 찾는다.
-     * 로그인 된 세션 ID로 유저ID를 찾아 계정 정보를 얻을 때 사용한다.
-     * @param username 유저 ID
-     * @return 계정 정보
-     */
-    public AccountDto loadUserByUsername(String username) throws UsernameNotFoundException {
-        return accountRepository.findByUsername(username)
-                .map(AccountDto::from)
-                .orElseThrow(() -> new ApplicationException(ErrorCode.USER_NOT_FOUND));
-    }
+    private final JwtTokenProvider jwtTokenProvider;
 
     /**
      * 유저 ID와 비밀번호를 확인해 계정 정보를 찾는다.
+     *
      * @param username 유저 ID
      * @param password 비밀번호
      * @return 계정정보
      */
-    public AccountDto login(String username, String password) {
+    public String login(String username, String password) {
         // 가입 여부 확인
-        AccountDto accountDto = loadUserByUsername(username);
+        AccountDto accountDto = accountRepository.findByUsername(username)
+                .map(AccountDto::from)
+                .orElseThrow(() -> new ApplicationException(ErrorCode.USER_NOT_FOUND));
 
         // 비밀번호를 암호화 시켜 저장된 비밀번호와 대조
         if (!passwordEncoder.matches(password, accountDto.getPassword())) {
             throw new ApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
-        // 계정 정보 반환
-        return accountDto;
+        // 토큰 반환
+        return jwtTokenProvider.createToken(accountDto.getUsername(), accountDto.getAuthorities());
     }
 
     // TODO: 회원 가입 비즈니스 로직 구현하기
     public AccountDto join() {
         String testUsername = "tester";
         String testPassword = "password";
-        Account testAccount = Account.of(testUsername, passwordEncoder.encode(testPassword));
+        Account testAccount = Account.of(testUsername, passwordEncoder.encode(testPassword), AccountRole.USER);
         return AccountDto.from(accountRepository.save(testAccount));
     }
 
