19fe44a148087bc2df9066c5ea90c68c229a7b47
TaemHam <tk4003950@gmail.com>
2023-02-16 22:36:39 +0900

    Feat: "JWT 인증 오류 관련 코드 추가"
    * "CustomAuthenticationEntryPoint.java"
    AuthenticationEntryPoint 를 상속받아 인증 필터에서 예외 발생시 어떻게 처리할지 정의해 놓은 클래스이다.
    
    * `ErrorCode.java`
    세션 관련 오류 코드를 삭제하고, JWT 관련 오류 코드를 추가했다.

--- src/main/java/com/filmdoms/community/account/exception/ErrorCode.java
+++ src/main/java/com/filmdoms/community/account/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러 발생."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
-    INVALID_SESSION_ID(HttpStatus.UNAUTHORIZED, "세션 ID가 일치하지 않습니다."),
+    AUTHENTICATION_ERROR(HttpStatus.UNAUTHORIZED, "인증 중 오류가 발생했습니다."),
     ;
 
     private final HttpStatus status;
