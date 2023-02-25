b00ae649cc900e19f8b243c44513948bf01509ab
TaemHam <tk4003950@gmail.com>
2023-02-25 10:23:48 +0900

    Fix: "CriticService 에 ImageFile 변경사항 적용"
    ImageFile 에 Url 제거한 변경사항이 CriticService에 적용되지 않아 버그를 일으켰다. ImageFile을 ImageFileDto로 변환하여 사용하는 것으로 버그를 고쳤다.

--- src/main/java/com/filmdoms/community/board/critic/service/CriticBoardService.java
+++ src/main/java/com/filmdoms/community/board/critic/service/CriticBoardService.java
@@ -1,51 +1,47 @@
 package com.filmdoms.community.board.critic.service;
 
-import com.filmdoms.community.account.data.constants.AccountRole;
 import com.filmdoms.community.account.data.dto.response.Response;
-import com.filmdoms.community.account.data.entity.Account;
 import com.filmdoms.community.account.exception.ApplicationException;
 import com.filmdoms.community.account.exception.ErrorCode;
 import com.filmdoms.community.account.repository.AccountRepository;
 import com.filmdoms.community.board.critic.data.dto.request.post.CriticBoardPostRequestDto;
-import com.filmdoms.community.board.critic.data.dto.request.post.CriticBoardUpdateRequestDto;
 import com.filmdoms.community.board.critic.data.dto.response.CriticBoardGetResponseDto;
 import com.filmdoms.community.board.critic.data.entity.CriticBoardHeader;
 import com.filmdoms.community.board.critic.repository.CriticBoardHeaderRepository;
 import com.filmdoms.community.board.data.BoardContent;
+import com.filmdoms.community.imagefile.data.dto.ImageFileDto;
+import com.filmdoms.community.imagefile.data.dto.UploadedFileDto;
 import com.filmdoms.community.imagefile.data.entitiy.ImageFile;
 import com.filmdoms.community.imagefile.repository.ImageFileRepository;
-import com.filmdoms.community.imagefile.service.AmazonS3Upload;
-import jakarta.annotation.PostConstruct;
+import com.filmdoms.community.imagefile.service.AmazonS3UploadService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CriticBoardService {
 
-        private final AccountRepository accountRepository;
-        private final CriticBoardHeaderRepository criticBoardHeaderRepository;
-        private final ImageFileRepository imageFileRepository;
-        private final AmazonS3Upload amazonS3Upload;
+    @Value("${domain}")
+    private String domain;
+    private final AccountRepository accountRepository;
+    private final CriticBoardHeaderRepository criticBoardHeaderRepository;
+    private final ImageFileRepository imageFileRepository;
+    private final AmazonS3UploadService amazonS3UploadService;
 
-        @PersistenceContext
-        private  final EntityManager em;
+    @PersistenceContext
+    private final EntityManager em;
 
 
-
-        public Response writeCritic(CriticBoardPostRequestDto dto, List<MultipartFile> multipartFileList) {
+    public Response writeCritic(CriticBoardPostRequestDto dto, List<MultipartFile> multipartFileList) {
 
         log.info("영화 작성 시작");
 
@@ -58,31 +54,27 @@ public class CriticBoardService {
                         .orElseThrow(() -> new ApplicationException(ErrorCode.URI_NOT_FOUND)))
                 .build();
 
-
         log.info("영화 작성 시작2");
 
         criticBoardHeaderRepository.save(criticBoardHeader);
 
-
         List<String> urlList = new ArrayList<>();
-        if(multipartFileList != null)
-        {
+        if (multipartFileList != null) {
             log.info("멑티파일리스트");
             multipartFileList.stream().forEach(multipartFile ->
             {
                 String originalFileName = null;
-                String uuidFileName = UUID.randomUUID().toString();
                 originalFileName = multipartFile.getOriginalFilename();
-                log.info("멀티파트리스트 사이즈{}",multipartFileList.size());
-                String url = null;
-                try {
-                    url = amazonS3Upload.upload(multipartFile, uuidFileName, originalFileName);
-                    ImageFile imageFile = ImageFile.from(uuidFileName, originalFileName,criticBoardHeader, url);
-                    imageFileRepository.save(imageFile);
-                    if(url!=null)
-                        log.info("파일 업로드 성공");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                log.info("멀티파트리스트 사이즈{}", multipartFileList.size());
+                UploadedFileDto uploadedFileDto = null;
+                uploadedFileDto = amazonS3UploadService.upload(multipartFile, originalFileName);
+                imageFileRepository.save(ImageFile.builder()
+                        .uuidFileName(uploadedFileDto.getUuidFileName())
+                        .originalFileName(uploadedFileDto.getOriginalFileName())
+                        .boardHeadCore(criticBoardHeader)
+                        .build());
+                if (uploadedFileDto.getUrl() != null) {
+                    log.info("파일 업로드 성공");
                 }
 
 
@@ -90,14 +82,12 @@ public class CriticBoardService {
 
         }
 
-
         return Response.success("sucess했음");
 
 
     }
 
-    public List<CriticBoardGetResponseDto> getCriticBoardList()
-    {
+    public List<CriticBoardGetResponseDto> getCriticBoardList() {
         List<CriticBoardHeader> resultBoard = getCriticBoardHeaders();
         List<ImageFile> imageFiles = getImageFiles(resultBoard);
 
@@ -111,8 +101,6 @@ public class CriticBoardService {
 
     }
 
-
-
 //    public Response updateCriticBoard(CriticBoardUpdateRequestDto dto, List<MultipartFile> multipartFiles)
 //    {
 //        TypedQuery<CriticBoardHeader> query = em.createQuery("SELECT c from CriticBoardHeader c join fetch c.boardContent where c.id =:id", CriticBoardHeader.class);
@@ -122,8 +110,7 @@ public class CriticBoardService {
 //        return Response.success(criticBoard);
 //    }
 
-    public String deleteCriticBoard()
-    {
+    public String deleteCriticBoard() {
 
         return "";
     }
@@ -144,15 +131,20 @@ public class CriticBoardService {
         return resultBoard;
     }
 
-    private  void setImageFilesToCriticBoardHeader(List<CriticBoardHeader> resultBoard, List<ImageFile> imageFiles, HashMap<Long, List<String>> imageFileHashMap, List<CriticBoardGetResponseDto> responseDtoList) {
+    private void setImageFilesToCriticBoardHeader(List<CriticBoardHeader> resultBoard, List<ImageFile> imageFiles,
+                                                  HashMap<Long, List<String>> imageFileHashMap,
+                                                  List<CriticBoardGetResponseDto> responseDtoList) {
         //초기 해시맵 셋팅
-        resultBoard.stream().forEach(criticBoardHeader -> imageFileHashMap.put(criticBoardHeader.getId(),new ArrayList<>()));
+        resultBoard.stream()
+                .forEach(criticBoardHeader -> imageFileHashMap.put(criticBoardHeader.getId(), new ArrayList<>()));
         //이미지 파일 모으기
-        imageFiles.stream().forEach(imageFile -> imageFileHashMap.get(imageFile.boardHeadCore.getId()).add(imageFile.getFileUrl()));
+        imageFiles.stream()
+                .map(imageFile -> ImageFileDto.from(imageFile, domain))
+                .forEach(dto -> imageFileHashMap.get(dto.getHeaderId()).add(dto.getFileUrl()));
         //responseDtoList 리스트에 add
-        resultBoard.stream().forEach(criticBoardHeader -> responseDtoList.add( CriticBoardGetResponseDto.from(criticBoardHeader, imageFileHashMap)));
+        resultBoard.stream().forEach(criticBoardHeader -> responseDtoList.add(
+                CriticBoardGetResponseDto.from(criticBoardHeader, imageFileHashMap)));
     }
-
 
 
 }
