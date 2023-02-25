d53776af98332f4f8d9ece2956b632f738177fd9
TaemHam <tk4003950@gmail.com>
2023-02-25 23:06:44 +0900

    Feat: "요청 데이터 유효성 검증 로직 추가"
    * `build.gradle`
    요청으로 들어오는 데이터를 검증해주기 위해 Spring Validation 관련 의존성을 추가했다.
    
    * `ErrorCode.java`
    요청 데이터 파싱 오류 코드를 추가했다.
    
    * `GlobalControllerAdvice.java`
    요청 데이터 파싱 오류를 처리해주는 로직을 추가했다.
    Validation에 걸리면 validExceptionHandler에서, 그 외의 상황은 JsonParseExceptionHandler에서 처리한다.

--- src/main/java/com/filmdoms/community/account/exception/GlobalControllerAdvice.java
+++ src/main/java/com/filmdoms/community/account/exception/GlobalControllerAdvice.java
@@ -1,9 +1,16 @@
 package com.filmdoms.community.account.exception;
 
 import com.filmdoms.community.account.data.dto.response.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,15 +20,37 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<?> applicationHandler(ApplicationException e) {
-        log.error("Error occurs {}", e.toString());
+        log.error("Error occurs: {}", e.toString());
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(Response.error(e.getErrorCode().name()));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> applicationHandler(RuntimeException e) {
-        log.error("Error occurs {}", e.toString());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        log.error("Error occurs: {}", e.toString());
+        return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
                 .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.name()));
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> JsonParseExceptionHandler(
+            HttpMessageNotReadableException e) {
+        log.error("Error occurs: {}", e.toString());
+        return ResponseEntity.status(ErrorCode.REQUEST_PARSE_ERROR.getStatus())
+                .body(Response.error(ErrorCode.REQUEST_PARSE_ERROR.name()));
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<?> validExceptionHandler(
+            MethodArgumentNotValidException e) {
+        log.error("Error occurs: {}", ErrorCode.REQUEST_PARSE_ERROR.getMessage());
+        // 파싱 중 Validation에 걸린 필드의 예외 메시지를 모두 모읍니다.
+        List<String> errors = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+        return ResponseEntity.status(ErrorCode.REQUEST_PARSE_ERROR.getStatus())
+                .body(Response.error(ErrorCode.REQUEST_PARSE_ERROR.name(), errors));
+    }
 }
+
