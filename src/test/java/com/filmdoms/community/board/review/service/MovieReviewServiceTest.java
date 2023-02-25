6ea7afade59da47b2b8e5754335bbb78c4a7ebc0
TaemHam <tk4003950@gmail.com>
2023-02-25 11:21:05 +0900

    Fix: "S3 업로드 서비스 반환 타입에 맞춰 서비스 테스트 수정"
    S3 업로드 서비스가 Url 스트링이 아닌, Dto를 반환하므로, 해당 수정사항에 맞춰 테스트를 수정했다.

--- src/test/java/com/filmdoms/community/board/review/service/MovieReviewServiceTest.java
+++ src/test/java/com/filmdoms/community/board/review/service/MovieReviewServiceTest.java
@@ -1,5 +1,9 @@
 package com.filmdoms.community.board.review.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+
 import com.filmdoms.community.account.data.constants.AccountRole;
 import com.filmdoms.community.account.data.entity.Account;
 import com.filmdoms.community.account.repository.AccountRepository;
@@ -8,10 +12,15 @@ import com.filmdoms.community.board.review.data.dto.request.MovieReviewCreateReq
 import com.filmdoms.community.board.review.data.dto.response.MovieReviewCreateResponseDto;
 import com.filmdoms.community.board.review.data.entity.MovieReviewHeader;
 import com.filmdoms.community.board.review.repository.MovieReviewHeaderRepository;
-import com.filmdoms.community.imagefile.service.AmazonS3Upload;
+import com.filmdoms.community.imagefile.data.dto.UploadedFileDto;
+import com.filmdoms.community.imagefile.service.AmazonS3UploadService;
 import com.filmdoms.community.imagefile.service.ImageFileService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -21,14 +30,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 
 @DataJpaTest
 @DisplayName("영화 리뷰 서비스-리포지토리 통합 테스트")
@@ -47,7 +48,7 @@ class MovieReviewServiceTest {
     AccountRepository accountRepository;
 
     @MockBean
-    AmazonS3Upload amazonS3Upload;
+    AmazonS3UploadService amazonS3UploadService;
 
     @PersistenceContext
     EntityManager em;
@@ -56,13 +57,15 @@ class MovieReviewServiceTest {
     public void 이미지_없는_영화리뷰_생성() throws IOException {
         //given
         Account testUser = accountRepository.save(Account.of("user1", "1234", AccountRole.USER));
-        MovieReviewCreateRequestDto requestDto = new MovieReviewCreateRequestDto(MovieReviewTag.A, "영화 리뷰 제목", testUser.getId(), "영화 리뷰 내용");
+        MovieReviewCreateRequestDto requestDto = new MovieReviewCreateRequestDto(MovieReviewTag.A, "영화 리뷰 제목",
+                testUser.getId(), "영화 리뷰 내용");
 
         //when
         MovieReviewCreateResponseDto responseDto = movieReviewService.createReview(requestDto, null);
 
         //then
-        MovieReviewHeader header = headerRepository.findById(responseDto.getPostId()).orElseThrow(() -> new RuntimeException("요청한 포스트 아이디가 존재하지 않음"));
+        MovieReviewHeader header = headerRepository.findById(responseDto.getPostId())
+                .orElseThrow(() -> new RuntimeException("요청한 포스트 아이디가 존재하지 않음"));
         assertThat(header.getAuthor().getId()).isEqualTo(testUser.getId()); //request DTO에서 전달된 값들이 저장되었는지 확인
         assertThat(header.getTitle()).isEqualTo(requestDto.getTitle());
         assertThat(header.getTag()).isEqualTo(requestDto.getTag());
@@ -72,14 +75,20 @@ class MovieReviewServiceTest {
     @Test
     public void 이미지_있는_영화리뷰_생성() throws IOException {
         //given
+        UploadedFileDto uploadedFileDto = UploadedFileDto.builder()
+                .uuidFileName("(randomUuidFileName).png")
+                .build();
         //리뷰 이미지 생성
         List<MultipartFile> multipartFiles = IntStream.range(0, 5).boxed()
-                .map(i -> (MultipartFile) new MockMultipartFile("image", "image_original_file_name_" + i, "image/jpeg", new byte[10])) //파일 크기가 0이면 저장되지 않으므로 10바이트짜리 파일 생성
+                .map(i -> (MultipartFile) new MockMultipartFile("image", "image_original_file_name_" + i, "image/jpeg",
+                        new byte[10])) //파일 크기가 0이면 저장되지 않으므로 10바이트짜리 파일 생성
                 .collect(Collectors.toList()); //가짜 멀티파트 이미지 5장 생성
 
         Account testUser = accountRepository.save(Account.of("user1", "1234", AccountRole.USER));
-        MovieReviewCreateRequestDto requestDto = new MovieReviewCreateRequestDto(MovieReviewTag.A, "영화 리뷰 제목", testUser.getId(), "영화 리뷰 내용");
-        Mockito.when(amazonS3Upload.upload(any(), any(), any())).thenReturn("fileUrl"); //amazonS3Upload 객체에 가짜 행동 주입
+        MovieReviewCreateRequestDto requestDto = new MovieReviewCreateRequestDto(MovieReviewTag.A, "영화 리뷰 제목",
+                testUser.getId(), "영화 리뷰 내용");
+        Mockito.when(amazonS3UploadService.upload(any(), any()))
+                .thenReturn(uploadedFileDto); //amazonS3Upload 객체에 가짜 행동 주입
 
         //when
         MovieReviewCreateResponseDto responseDto = movieReviewService.createReview(requestDto, multipartFiles);
@@ -88,7 +97,8 @@ class MovieReviewServiceTest {
 
         //then
         assertThat(multipartFiles).size().isEqualTo(5); //멀티파트 파일은 5개
-        MovieReviewHeader header = headerRepository.findById(responseDto.getPostId()).orElseThrow(() -> new RuntimeException("요청한 포스트 아이디가 존재하지 않음"));
+        MovieReviewHeader header = headerRepository.findById(responseDto.getPostId())
+                .orElseThrow(() -> new RuntimeException("요청한 포스트 아이디가 존재하지 않음"));
         assertThat(header.getImageFiles().size()).isEqualTo(multipartFiles.size()); //헤더에 ImageFile 객체 5개가 연결되었는지 확인
         assertThat(header.getAuthor().getId()).isEqualTo(testUser.getId());
         assertThat(header.getTitle()).isEqualTo(requestDto.getTitle());
