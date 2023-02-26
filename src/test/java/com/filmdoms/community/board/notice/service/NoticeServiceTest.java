46a283dc7a07d2cbe49a5af971e9a22b74947a24
Geon <gunner6603@snu.ac.kr>
2023-02-26 15:58:21 +0900

    Fix: 테스트에 JPA Auditing 적용되지 않는 버그 수정
    DataJpaTest는 설정 컴포넌트를 스캔하지 않으므로 직접 포함해주어야 한다.
    
    * `DataJpaTestWithJpaAuditing.java`
    @DataJpaTest (JpaAuditing 설정 컴포넌트 포함)
    @ActiveProfiles("test")
    와 같이 리포지토리 테스트에 필요한 어노테이션을 모아 한 번에 적용할 수 있도록 만든 어노테이션
    
    * 나머지 테스트 파일은 위 어노테이션을 적용하도록 변경
    
    resolves #47

--- src/test/java/com/filmdoms/community/board/notice/service/NoticeServiceTest.java
+++ src/test/java/com/filmdoms/community/board/notice/service/NoticeServiceTest.java
@@ -1,14 +1,10 @@
 package com.filmdoms.community.board.notice.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-
 import com.filmdoms.community.account.data.constants.AccountRole;
 import com.filmdoms.community.account.data.entity.Account;
 import com.filmdoms.community.account.exception.ApplicationException;
 import com.filmdoms.community.account.repository.AccountRepository;
+import com.filmdoms.community.annotation.DataJpaTestWithJPAAuditing;
 import com.filmdoms.community.board.notice.data.dto.request.NoticeCreateRequestDto;
 import com.filmdoms.community.board.notice.data.dto.response.NoticeCreateResponseDto;
 import com.filmdoms.community.board.notice.data.entity.NoticeHeader;
@@ -18,24 +14,27 @@ import com.filmdoms.community.imagefile.service.AmazonS3UploadService;
 import com.filmdoms.community.imagefile.service.ImageFileService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
-@DataJpaTest
-@ActiveProfiles("test")
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+
+@DataJpaTestWithJPAAuditing
 @DisplayName("공지 서비스-리포지토리 통합 테스트")
 class NoticeServiceTest {
 
