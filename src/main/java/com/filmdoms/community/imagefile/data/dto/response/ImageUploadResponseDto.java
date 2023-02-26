515c068cbd0d154d7df4ffa3c30f6dcff2659446
Geon <gunner6603@snu.ac.kr>
2023-02-26 23:53:12 +0900

    Feat: 이미지 업로드 관련 DTO, 컨트롤러 메서드, 서비스 메서드 추가
    * `ImageUploadResponseDto.java`
    이미지 업로드 처리 후 응답을 위한 DTO
    
    * `FileUploadController.java`
    멀티파트 이미지 데이터 처리를 위한 컨트롤러 메서드
    (다수 데이터를 한꺼번에 처리)
    
    * `AmazonS3UploadService.java`
    uploadAndSaveImages 메서드는 멀티파트 이미지들을 순차적으로 업로드/저장 처리함

---
+++ src/main/java/com/filmdoms/community/imagefile/data/dto/response/ImageUploadResponseDto.java
@@ -0,0 +1,16 @@
+package com.filmdoms.community.imagefile.data.dto.response;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ImageUploadResponseDto {
+
+    private List<Long> imageIds;
+
+    public ImageUploadResponseDto(List<Long> imageIds) {
+        this.imageIds = imageIds;
+    }
+}
