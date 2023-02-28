6f27c865d0c42d1265ad8a9b5ba7e888743997a9
Geon <gunner6603@snu.ac.kr>
2023-02-28 21:10:39 +0900

    Feat: 공지 컨트롤러에 RUD 메서드 작성
    * RUD 기능을 각각 GET, PUT, DELETE 메소드로 처리하도록 구현

--- src/main/java/com/filmdoms/community/board/notice/controller/NoticeController.java
+++ src/main/java/com/filmdoms/community/board/notice/controller/NoticeController.java
@@ -3,15 +3,17 @@ package com.filmdoms.community.board.notice.controller;
 import com.filmdoms.community.account.data.dto.AccountDto;
 import com.filmdoms.community.account.data.dto.response.Response;
 import com.filmdoms.community.board.notice.data.dto.request.NoticeCreateRequestDto;
+import com.filmdoms.community.board.notice.data.dto.request.NoticeUpdateRequestDto;
 import com.filmdoms.community.board.notice.data.dto.response.NoticeCreateResponseDto;
+import com.filmdoms.community.board.notice.data.dto.response.NoticeDetailResponseDto;
 import com.filmdoms.community.board.notice.data.dto.response.NoticeMainPageDto;
+import com.filmdoms.community.board.notice.data.dto.response.NoticeUpdateResponseDto;
 import com.filmdoms.community.board.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -31,6 +33,25 @@ public class NoticeController {
     @PostMapping
     public Response<NoticeCreateResponseDto> create(@RequestBody NoticeCreateRequestDto requestDto, @AuthenticationPrincipal AccountDto accountDto) {
         NoticeCreateResponseDto responseDto = noticeService.create(requestDto, accountDto);
+        return Response.success(responseDto);
+    }
+
+    @GetMapping("/{noticeId}")
+    public Response<NoticeDetailResponseDto> detail(@PathVariable Long noticeId) {
+        NoticeDetailResponseDto responseDto = noticeService.getDetail(noticeId);
+        return Response.success(responseDto);
+    }
+
+    @DeleteMapping("/{noticeId}")
+    public Response delete(@PathVariable Long noticeId) {
+        noticeService.deleteNotice(noticeId);
+        return Response.success();
+    }
+
+    @PutMapping("/{noticeId}")
+    public Response<NoticeUpdateResponseDto> update(@PathVariable Long noticeId, @RequestBody NoticeUpdateRequestDto requestDto) {
+        log.info("{}", requestDto.getContentImageId());
+        NoticeUpdateResponseDto responseDto = noticeService.updateNotice(noticeId, requestDto);
         return Response.success(responseDto);
     }
 
