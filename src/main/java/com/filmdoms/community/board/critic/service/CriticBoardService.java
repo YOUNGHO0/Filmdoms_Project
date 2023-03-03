d453a63592edbf137731f1467cda9251b34370a6
YOUNGHO0 <31pqopq13@gmail.com>
2023-03-03 20:35:19 +0900

    Refactor : "해시맵 사용하던CriticBoardService 서비스 코드 Stream 으로 수정"

--- src/main/java/com/filmdoms/community/board/critic/service/CriticBoardService.java
+++ src/main/java/com/filmdoms/community/board/critic/service/CriticBoardService.java
@@ -9,27 +9,24 @@ import com.filmdoms.community.account.repository.AccountRepository;
 import com.filmdoms.community.board.critic.data.dto.request.post.CriticBoardPostRequestDto;
 import com.filmdoms.community.board.critic.data.dto.request.post.CriticBoardUpdateRequestDto;
 import com.filmdoms.community.board.critic.data.dto.response.CriticBoardGetResponseDto;
+import com.filmdoms.community.board.critic.data.dto.response.CriticBoardSinglePageResponseDto;
 import com.filmdoms.community.board.critic.data.entity.CriticBoardHeader;
 import com.filmdoms.community.board.critic.repository.CriticBoardHeaderRepository;
 import com.filmdoms.community.board.data.BoardContent;
-import com.filmdoms.community.imagefile.data.dto.ImageFileDto;
 import com.filmdoms.community.imagefile.data.entitiy.ImageFile;
 import com.filmdoms.community.imagefile.repository.ImageFileRepository;
 import com.filmdoms.community.imagefile.service.ImageFileService;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -50,7 +47,7 @@ public class CriticBoardService {
 
     public Response writeCritic(CriticBoardPostRequestDto dto) {
 
-        log.info("영화 작성 시작");
+
         BoardContent criticBoardContent = BoardContent.builder().content(dto.getContent()).build();
         CriticBoardHeader criticBoardHeader = CriticBoardHeader.builder()
                 .preHeader(dto.getPreHeader())
@@ -63,11 +60,7 @@ public class CriticBoardService {
                 .mainImage(imageFileRepository.getReferenceById(dto.getMainImageId()))
                 .build();
 
-        log.info("영화 작성 시작2");
-
         criticBoardHeaderRepository.save(criticBoardHeader);
-
-        imageFileService.setImageContent(dto.getMainImageId(), criticBoardContent);
         imageFileService.setImageContent(dto.getContentImageId(),criticBoardContent);
 
 
@@ -76,44 +69,28 @@ public class CriticBoardService {
 
     }
 
-    public List<CriticBoardGetResponseDto> getCriticBoardList() {
-        List<CriticBoardHeader> resultBoard = criticBoardHeaderRepository.getBoardList();
-        List<BoardContent> boardcontents = resultBoard.stream().map(criticBoardHeader -> criticBoardHeader.getBoardContent()).collect(Collectors.toList());
+    public List<CriticBoardGetResponseDto> getCriticBoardLists() {
 
-        List<ImageFile> imageFiles = imageFileRepository.getImageFiles(boardcontents);
+        List<CriticBoardHeader> boardList = criticBoardHeaderRepository.findCriticBoardHeaderWithAuthorContentMainImage();
+        List<CriticBoardGetResponseDto> dto = boardList.stream().map(criticBoardHeader -> CriticBoardGetResponseDto.from(criticBoardHeader, domain)).collect(Collectors.toList());
 
-        HashMap<Long, List<String>> imageFileHashMap = new HashMap<>();
-        List<CriticBoardGetResponseDto> responseDtoList = new ArrayList<>();
-        setImageUrltoHeader (resultBoard, imageFiles, imageFileHashMap, responseDtoList);
-        return responseDtoList;
-
-
+        return dto;
     }
 
-    private void setImageUrltoHeader (List<CriticBoardHeader> resultBoard, List<ImageFile> imageFiles, HashMap<Long, List<String>> imageFileHashMap, List<CriticBoardGetResponseDto> responseDtoList) {
+    public CriticBoardSinglePageResponseDto getSingleCriticBoardPage(Long id) {
 
-        // 해시맵을 게시판의 id를 키로 하여 value 를List<String> 즉 url 리스트로 가지는 해시맵 생성
-        //초기 해시맵 세팅
-        resultBoard.stream()
-                .forEach(criticBoardHeader -> imageFileHashMap.put(criticBoardHeader.getId(), new ArrayList<>()));
-        //이미지 파일 모으기
-        // 해당하는 게시판 번호로 이미지 파일 맵에 넣어주기
-        imageFiles.stream()
-                .map(imageFile -> ImageFileDto.from(imageFile, domain))
-                .forEach(dto -> imageFileHashMap.get(dto.getContentId()).add(dto.getFileUrl()));
-        // 최종적으로 dto에 이미지 파일 리스트를 넣어주기
-        //responseDtoList 리스트에 add
-        resultBoard.stream().forEach(criticBoardHeader -> responseDtoList.add(
-                CriticBoardGetResponseDto.from(criticBoardHeader, imageFileHashMap)));
+        CriticBoardHeader singlePage = criticBoardHeaderRepository.findCriticBoardHeaderWithAuthorContentMainImageById(id);
+        return CriticBoardSinglePageResponseDto.from(singlePage,domain);
     }
 
 
     public Response updateCriticBoard(CriticBoardUpdateRequestDto dto)
     {
-        CriticBoardHeader criticBoard = criticBoardHeaderRepository.getCriticBoardHeaderWithBoardContent(dto.getId());
+
         ImageFile mainImageFile = imageFileRepository.findById(dto.getMainImageId())
                 .orElseThrow(() -> new ApplicationException(ErrorCode.INVALID_IMAGE_ID));
         //이미지 파일 업데이트 코드(헤더쪽)
+        CriticBoardHeader criticBoard = criticBoardHeaderRepository.findCriticBoardHeaderWithAuthorContentMainImageById(dto.getId());
         criticBoard.updateCriticBoard(dto.getTitle(), dto.getContent(), dto.getPreHeader(),mainImageFile);
         //이미지 파일 업데이트 코드 (이미지 파일쪽)
         imageFileService.updateImageContent(dto.getContentImageId(),criticBoard.getBoardContent());
@@ -140,14 +117,14 @@ public class CriticBoardService {
 
 
 
-    @PostConstruct
-    public void test()
-    {
-        Account author = Account.builder()
-                .username("user1").password("123456").role(AccountRole.USER).build();
-        accountRepository.save(author);
-        //저장 되는지 확인
-    }
+//    @PostConstruct
+//    public void test()
+//    {
+//        Account author = Account.builder()
+//                .username("user1").password("123456").role(AccountRole.USER).build();
+//        accountRepository.save(author);
+//        //저장 되는지 확인
+//    }
 
 
 }
