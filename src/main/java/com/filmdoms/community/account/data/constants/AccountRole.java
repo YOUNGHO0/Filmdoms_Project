bf9b23c31ae028e269454a455002571aee4e55fd
Geon <gunner6603@snu.ac.kr>
2023-03-17 19:46:58 +0900

    Feat: enum 클래스 추가
    * `AccountRole.java`
    추가 정보를 기입하여 회원가입을 끝마치기 전 상태인 GUEST 추가
    
    * `OAuthType.java`
    SuccessHandler에서 프론트에 응답을 내려줄 때, 회원가입을 해서 추가 정보가 필요한 경우 SIGNUP을, 로그인을 한 경우 LOGIN을 표시하기 위해 생성

--- src/main/java/com/filmdoms/community/account/data/constants/AccountRole.java
+++ src/main/java/com/filmdoms/community/account/data/constants/AccountRole.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AccountRole {
     USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN");
+    ADMIN("ROLE_ADMIN"),
+    GUEST("ROLE_GUEST");
 
     private final String name;
 }
