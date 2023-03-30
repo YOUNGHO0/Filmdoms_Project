863271441277491b5ead237765bfdbd28a8bd57b
Geon <gunner6603@snu.ac.kr>
2023-03-31 00:01:37 +0900

    Feat: 리포지토리 생성
    새로 생성한 엔티티에 대한 리포지토리를 뼈대만 생성함
    Content 엔티티는 Article 엔티티와 생명주기가 동일하므로 cascade 옵션을 주었고, 조회 시에도 Article과 함께 조회되므로 리포지토리를 생성하지 않음

---
+++ src/main/java/com/filmdoms/community/file/repository/FileRepository.java
@@ -0,0 +1,7 @@
+package com.filmdoms.community.file.repository;
+
+import com.filmdoms.community.file.data.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<File, Long> {
+}
