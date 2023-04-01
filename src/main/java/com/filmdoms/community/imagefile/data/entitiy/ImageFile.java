599710178acf2cbfc9dde2428a3ee0e2c3a6389a
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-01 14:16:04 +0900

    Fix: "시큐리티 설정, 이미지파일 변경" * join 과 login 이 api/v1이 안 붙어 있어서 해당 부분 수정함 * Critic을 삭제하였어서 해당 부분 의존성 삭제함

--- src/main/java/com/filmdoms/community/imagefile/data/entitiy/ImageFile.java
+++ src/main/java/com/filmdoms/community/imagefile/data/entitiy/ImageFile.java
@@ -1,22 +1,15 @@
 package com.filmdoms.community.imagefile.data.entitiy;
 
 
-import com.filmdoms.community.board.critic.data.entity.CriticBoardHeader;
 import com.filmdoms.community.board.data.BaseTimeEntity;
 import com.filmdoms.community.board.data.BoardContent;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import java.util.Objects;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Getter
 @Entity
