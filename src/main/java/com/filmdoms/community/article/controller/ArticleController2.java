ab2848333d903704396e1cb7d048d36ec1356e9b
Geon <gunner6603@snu.ac.kr>
2023-04-03 00:21:26 +0900

    Feat: 컨트롤러 작성
    * 메인 페이지 요청을 받는 readMain 메서드와 임시 데이터 생성 요청을 받는 initData 메서드를 가짐
    
    * 기존 컨트롤러에 `@RequestMapping ("/api/v1/article/{category}/{tag}")`이 붙어 있으므로 임시로 만든 컨트롤러 -> 태그를 url에 넣을지 논의 필요
    
    resolves: #88

---
+++ src/main/java/com/filmdoms/community/article/controller/ArticleController2.java
@@ -0,0 +1,34 @@
+package com.filmdoms.community.article.controller;
+
+import com.filmdoms.community.account.data.dto.response.Response;
+import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.data.dto.response.MainPageResponseDto;
+import com.filmdoms.community.article.service.ArticleService;
+import com.filmdoms.community.article.service.InitService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/article")
+@RequiredArgsConstructor
+public class ArticleController2 {
+
+    private final ArticleService articleService;
+    private final InitService initService;
+
+    @GetMapping("/{category}/main-page")
+    public Response<List<? extends MainPageResponseDto>> readMain(@PathVariable Category category, @RequestParam(defaultValue = "5") int limit) {
+        List<? extends MainPageResponseDto> dtoList = articleService.getMainPageDtoList(category, limit);
+        return Response.success(dtoList);
+    }
+
+    @GetMapping("/init-data")
+    public Response initData(@RequestParam(defaultValue = "10") int limit) {
+        initService.makeArticleData(limit);
+        return Response.success();
+    }
+}
