79d90aecf4a87827cd050aff96e4ca1c56bc6265
Geon <gunner6603@snu.ac.kr>
2023-04-02 23:23:00 +0900

    Feat: 일부 엔티티 수정
    * `Account.java`
    빌더에서 Role이 설정되지 않은 경우 AccountRole을 USER로 설정하는 로직을 가독성 향상을 위해 수정
    
    * `Article.java`
    댓글 리스트 매핑 추가
    
    * `Notice.java`
    mainImage 필드 추가
    article 필드에 옵션 추가
    > cascade 옵션으로 Notice가 저장/삭제될 때 Article도 같이 삭제되도록 함
    > `nullable = false` 옵션은 데이터베이스 not null 제약조건 생성
    > `optional = false` 옵션은 런타임에 매핑이 항상 존재하도록 강제함
    
    * `NewComment.java`
    댓글에도 추천 기능이 있으므로 추천수 필드 생성

--- src/main/java/com/filmdoms/community/article/data/entity/extra/Notice.java
+++ src/main/java/com/filmdoms/community/article/data/entity/extra/Notice.java
@@ -1,46 +1,47 @@
 package com.filmdoms.community.article.data.entity.extra;
 
 import com.filmdoms.community.article.data.entity.Article;
+import com.filmdoms.community.file.data.entity.File;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 
 @Entity
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JoinColumn(name = "article_id", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.ALL)
+    private Article article;
+
+    @JoinColumn(name = "file_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private File mainImage;
+
     private LocalDateTime startDate;
+
     private LocalDateTime endDate;
 
-
-    @OneToOne
-    @JoinColumn(name = "article_id")
-    Article article;
-
     @Builder
-    private Notice(Article article,LocalDateTime startDate, LocalDateTime endDate) {
+    private Notice(Article article, File mainImage, LocalDateTime startDate, LocalDateTime endDate) {
         this.article = article;
+        this.mainImage = mainImage;
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
-    public static Notice from(Article article, LocalDateTime startDate, LocalDateTime endDate)
-    {
-      return  Notice.builder()
+    public static Notice from(Article article, LocalDateTime startDate, LocalDateTime endDate) {
+        return  Notice.builder()
                 .article(article)
                 .startDate(startDate)
                 .endDate(endDate)
                 .build();
-
     }
-
-
 }
