36836c0a29161daa55203e1ef25e57aeb666e657
Geon <gunner6603@snu.ac.kr>
2023-04-03 00:08:59 +0900

    Feat: 서비스 메서드 추가
    * `getMainPageDtoList`는 category와 limit 파라미터를 받아서 해당 카테고리 메인 페이지 DTO를 limit 개수만큼 반환함
    
    * Notice, Critic은 해당 엔티티와 매핑이 존재한다는 사실만으로 Article의 카테고리를 알 수 있으나, Movie 카테고리의 경우 Article 조회 시 카테고리 정보를 주어야 함
    
    * 예외의 경우 발생할 일이 없으나 넣어 놓음

--- src/main/java/com/filmdoms/community/article/service/ArticleService.java
+++ src/main/java/com/filmdoms/community/article/service/ArticleService.java
@@ -1,21 +1,37 @@
 package com.filmdoms.community.article.service;
 
 import com.filmdoms.community.account.data.dto.response.Response;
+import com.filmdoms.community.account.exception.ApplicationException;
+import com.filmdoms.community.account.exception.ErrorCode;
+import com.filmdoms.community.article.data.constant.Category;
 import com.filmdoms.community.article.data.dto.ArticleControllerToServiceDto;
 import com.filmdoms.community.article.data.dto.notice.NoticeControllerToServiceDto;
+import com.filmdoms.community.article.data.dto.response.ArticleMainPageResponseDto;
+import com.filmdoms.community.article.data.dto.response.CriticMainPageResponseDto;
+import com.filmdoms.community.article.data.dto.response.MainPageResponseDto;
+import com.filmdoms.community.article.data.dto.response.NoticeMainPageResponseDto;
 import com.filmdoms.community.article.data.entity.Article;
+import com.filmdoms.community.article.data.entity.extra.Critic;
 import com.filmdoms.community.article.data.entity.extra.Notice;
 import com.filmdoms.community.article.repository.ArticleRepository;
+import com.filmdoms.community.article.repository.CriticRepository;
 import com.filmdoms.community.article.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
     private final NoticeRepository noticeRepository;
+    private final CriticRepository criticRepository;
 
     public Response createDefaultArticle(ArticleControllerToServiceDto dto)
     {
@@ -33,4 +49,29 @@ public class ArticleService {
         return Response.success(savedNotice.getId());
     }
 
+    public List<? extends MainPageResponseDto> getMainPageDtoList(Category category, int limit) {
+
+        if(category == Category.MOVIE) {
+            List<Article> articles = articleRepository.findByCategory(category, PageRequest.of(0, limit, Sort.by(Sort.Direction.DESC, "id"))); //Article의 id로 정렬
+
+            return articles.stream()
+                    .map(ArticleMainPageResponseDto::from)
+                    .toList(); //commentNum은 batch_size를 이용하여 쿼리 1번으로 구해짐
+
+        } else if(category == Category.FILM_UNIVERSE) {
+            List<Notice> notices = noticeRepository.findAllWithArticleAuthorMainImage(PageRequest.of(0, limit, Sort.by(Sort.Direction.DESC, "id"))); //category 정보 필요x, Notice의 id로 정렬
+
+            return notices.stream()
+                    .map(NoticeMainPageResponseDto::from)
+                    .toList();
+
+        } else if(category == Category.CRITIC) {
+            List<Critic> critics = criticRepository.findAllWithArticleAuthorMainImageContent(PageRequest.of(0, limit, Sort.by(Sort.Direction.DESC, "id")));//category 정보 필요x, Critic의 id로 정렬
+
+            return critics.stream()
+                    .map(CriticMainPageResponseDto::from)
+                    .toList();
+        }
+        throw new ApplicationException(ErrorCode.CATEGORY_NOT_FOUND);
+    }
 }
