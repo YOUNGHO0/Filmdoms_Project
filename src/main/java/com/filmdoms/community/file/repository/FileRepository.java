808f0f38a44284c38bd72b876cd23b3d8af2ce1c
Geon <gunner6603@snu.ac.kr>
2023-04-03 23:09:44 +0900

    Feat: 리포지토리 메서드 생성
    * `ArticleRepository.java`
    findAllReturnList 메서드 : 메인 페이지 recent 게시물 리스트 조회 용도, 주석 참고
    findByIdWithAuthorProfileImageContent 메서드 : 영화/비평 카테고리 상세 조회 용도, 페치 조인으로 연관 엔티티를 가져옴, 컬렉션은 navigation이 불가능하므로(예를 들어 article.comments.author와 같이 조회 불가) 컬렉션 조회는 모두 따로 수행함
    
    * `NoticeRepository.java`
    findByArticleIdWithArticleAuthorProfileImageContent 메서드 : 공지 카테고리 상세 조회 용도, join이 한 번 더 일어나는 점을 제외하면 ArticleRepository의 메서드와 동일
    
    * `FileRepository.java`
    articleId로 연관 File 객체를 조회하는 메서드 추가
    contentId로 조회하는 편이 조인이 덜 일어나지만 메서드의 범용성을 위해 articleId로 조회하도록 함
    
    * `NewCommentRepository.java`
    articleId로 연관 comment 객체를 조회하는 메서드 추가
    페치 조인으로 댓글 작성자, 댓글 작성자의 프로필 이미지까지 가져옴

--- src/main/java/com/filmdoms/community/file/repository/FileRepository.java
+++ src/main/java/com/filmdoms/community/file/repository/FileRepository.java
@@ -2,6 +2,16 @@ package com.filmdoms.community.file.repository;
 
 import com.filmdoms.community.file.data.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FileRepository extends JpaRepository<File, Long> {
+
+    @Query("SELECT f FROM Article a " +
+            "JOIN a.content.fileContents fc " +
+            "JOIN fc.file f " +
+            "WHERE a.id = :articleId")
+    List<File> findByArticleId(@Param("articleId") Long articleId);
 }
