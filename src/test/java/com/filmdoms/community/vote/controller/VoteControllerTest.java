5d7077ba01526ccb74014ad7d3563a0f7dc19bbc
TaemHam <tk4003950@gmail.com>
2023-04-03 16:55:18 +0900

    Test: "추천 기능 테스트 코드 작성"
    * `VoteControllerTest.java`
    추천 기능의 다음 경우에 대해 테스트 코드를 작성했다.
      * 게시글을 추천 요청시, 정상적인 요청이라면, 게시글의 총 추천 수와 나의 추천 여부를 반환한다.
      * 게시글을 추천 요청시, 존재하지 않는 게시글이라면, 에러를 반환한다.
      * 게시글을 추천 요청시, 미인증 유저라면, 인증 에러를 반환한다.
    
    * `VoteServiceTest.java`
    추천 기능 서비스 로직의 다음 경우에 대해 테스트 코드를 작성했다.
      * 추천 요청이 들어올 때, 이미 추천한 게시글이면, 추천을 삭제한 결과를 반환한다.
      * 추천 요청이 들어올 때, 아직 추천하지 않은 게시글이면, 추천을 추가한 결과를 반환한다.
      * 추천 요청이 들어올 때, 요청 게시글이 존재하지 않는다면, 예외를 발생시킨다.

---
+++ src/test/java/com/filmdoms/community/vote/controller/VoteControllerTest.java
@@ -0,0 +1,94 @@
+package com.filmdoms.community.vote.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.filmdoms.community.account.exception.ApplicationException;
+import com.filmdoms.community.account.exception.ErrorCode;
+import com.filmdoms.community.config.TestSecurityConfig;
+import com.filmdoms.community.vote.data.dto.VoteResponseDto;
+import com.filmdoms.community.vote.service.VoteService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(VoteController.class)
+@Import(TestSecurityConfig.class)
+@DisplayName("컨트롤러 - 추천 기능")
+class VoteControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private VoteService voteService;
+
+    @Test
+    @WithUserDetails(value = "testUser", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("게시글을 추천 요청시, 정상적인 요청이라면, 게시글의 총 추천 수와 나의 추천 여부를 반환한다.")
+    void givenProperRequest_whenVotingArticle_thenReturnsVoteResult() throws Exception {
+
+        VoteResponseDto responseDto = VoteResponseDto.builder()
+                .isVoted(true)
+                .voteCount(1)
+                .build();
+        // Given
+        given(voteService.toggleVote(any(), any())).willReturn(responseDto);
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/article/1/vote"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[?(@.resultCode == 'SUCCESS')]").exists())
+                .andExpect(jsonPath("$.result.isVoted").exists())
+                .andExpect(jsonPath("$.result.voteCount").exists());
+    }
+
+    @Test
+    @WithUserDetails(value = "testUser", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("게시글을 추천 요청시, 존재하지 않는 게시글이라면, 에러를 반환한다.")
+    void givenInvalidPostId_whenVotingArticle_thenReturnsUriNotFoundErrorCode() throws Exception {
+
+        // Given
+        doThrow(new ApplicationException(ErrorCode.URI_NOT_FOUND))
+                .when(voteService).toggleVote(any(), any());
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/article/1/vote"))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[?(@.resultCode == 'URI_NOT_FOUND')]").exists())
+                .andExpect(jsonPath("$[?(@.result == null)]").exists());
+    }
+
+    @Test
+    @WithAnonymousUser()
+    @DisplayName("게시글을 추천 요청시, 미인증 유저라면, 인증 에러를 반환한다.")
+    void givenUnauthenticatedUser_whenVotingArticle_thenReturnsUnauthenticatedErrorCode() throws Exception {
+
+        // Given
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/article/1/vote"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[?(@.result == null)]").exists())
+                .andExpect(jsonPath("$[?(@.resultCode == 'AUTHENTICATION_ERROR')]").exists());
+    }
+
+}
