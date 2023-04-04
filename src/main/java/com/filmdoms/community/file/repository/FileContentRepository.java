fcf47f45fc977f4398b63ae5b96f7d7c0c069ea8
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-04 13:07:32 +0900

    Feat : "File 객체로 FileContent 를 찾는 메서드 추가" * 해당 파일이 FileContent에 들어 있다면 해당 파일은 이미 등록된 파일이라고 볼 수 있다. * 따라서 예외처리를 위해 파일컨텐트 안을 확인해 봐야 하므로 해당 메서드를 추가하였다.

--- src/main/java/com/filmdoms/community/file/repository/FileContentRepository.java
+++ src/main/java/com/filmdoms/community/file/repository/FileContentRepository.java
@@ -1,7 +1,12 @@
 package com.filmdoms.community.file.repository;
 
+import com.filmdoms.community.file.data.entity.File;
 import com.filmdoms.community.file.data.entity.FileContent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FileContentRepository extends JpaRepository<FileContent, Long> {
+
+    Optional<FileContent> findByFile(File file);
 }
