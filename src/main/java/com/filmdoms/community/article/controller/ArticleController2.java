7c07ad9d36cd06466a8917caa2732260cd8a8cda
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-06 21:42:06 +0900

    Feat : "게시판 리스트 요청을 받는 컨트롤러 메서드 구현" * 게시판 리스트 요청을 받는 컨트롤러를 구현하였다. * 해당 컨트롤러 메서드에서 페이징 사이즈, 페이지 번호 등을 체크하여 올바르지 않는경우 Error를 리턴한다.

--- src/main/java/com/filmdoms/community/article/controller/ArticleController2.java
+++ src/main/java/com/filmdoms/community/article/controller/ArticleController2.java
@@ -2,7 +2,9 @@ package com.filmdoms.community.article.controller;
 
 import com.filmdoms.community.account.data.dto.AccountDto;
 import com.filmdoms.community.account.data.dto.response.Response;
+import com.filmdoms.community.account.exception.ErrorCode;
 import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.data.dto.response.boardlist.ParentBoardListDto;
 import com.filmdoms.community.article.data.dto.response.detail.ArticleDetailResponseDto;
 import com.filmdoms.community.article.data.dto.response.mainpage.MovieAndRecentMainPageResponseDto;
 import com.filmdoms.community.article.data.dto.response.mainpage.ParentMainPageResponseDto;
@@ -10,6 +12,7 @@ import com.filmdoms.community.article.service.ArticleService;
 import com.filmdoms.community.article.service.InitService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -46,5 +49,23 @@ public class ArticleController2 {
     public Response initData(@RequestParam(defaultValue = "10") int limit) {
         initService.makeArticleData(limit);
         return Response.success();
+    }
+
+    @GetMapping("/{category}")
+    public Response getBoardCategoryList(@PathVariable Category category, @RequestParam(defaultValue = "24") int size,
+                                         @RequestParam(defaultValue = "0") int page )
+    {
+        if(size >30) // 악의적 공격 방지
+            size = 30;
+
+        Page<? extends ParentBoardListDto> boardList = articleService.getBoardList(category, size, page);
+
+        if(boardList == null)
+            return Response.error(ErrorCode.CATEGORY_NOT_FOUND.getMessage());
+
+        if(boardList.getTotalPages()-1 < page)
+            return Response.error(ErrorCode.INVALID_Page_NUMBER.getMessage());
+
+        return Response.success(boardList);
     }
 }
