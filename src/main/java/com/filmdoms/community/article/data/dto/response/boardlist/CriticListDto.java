0298da11b93b7a68a05859f8e4b1cb6d42a4b81f
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-06 21:33:32 +0900

    Feat : "자식 게시판 리스트 Dto 작성" * 부모 게시판 Dto를 상속받아 각 자식 게시판 Dto를 만들었다. * 현재는 게시판 종류에 따라 리스트가 달라지는 시안은 없어서 모두 Article 의 형태만 갖는 상태로 구현하였다.

---
+++ src/main/java/com/filmdoms/community/article/data/dto/response/boardlist/CriticListDto.java
@@ -0,0 +1,13 @@
+package com.filmdoms.community.article.data.dto.response.boardlist;
+
+import com.filmdoms.community.article.data.entity.Article;
+
+public class CriticListDto extends ParentBoardListDto{
+    public CriticListDto(Article article) {
+        super(article);
+    }
+    public static CriticListDto  from(Article article)
+    {
+        return new CriticListDto(article);
+    }
+}
