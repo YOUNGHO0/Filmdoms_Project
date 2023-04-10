53de95e05a90af05507ca020317bb536e57da139
Geon <gunner6603@snu.ac.kr>
2023-04-10 13:56:35 +0900

    Feat: 컨트롤러 메서드 작성
    통일성을 위해 조회 api 경로를 /article/{category}/{articleId}/comment로 변경

--- src/main/java/com/filmdoms/community/newcomment/controller/NewCommentController.java
+++ src/main/java/com/filmdoms/community/newcomment/controller/NewCommentController.java
@@ -3,7 +3,10 @@ package com.filmdoms.community.newcomment.controller;
 import com.filmdoms.community.account.data.dto.AccountDto;
 import com.filmdoms.community.account.data.dto.response.Response;
 import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.newcomment.data.dto.request.NewCommentCreateRequestDto;
+import com.filmdoms.community.newcomment.data.dto.request.NewCommentUpdateRequestDto;
 import com.filmdoms.community.newcomment.data.dto.response.DetailPageCommentResponseDto;
+import com.filmdoms.community.newcomment.data.dto.response.NewCommentCreateResponseDto;
 import com.filmdoms.community.newcomment.service.NewCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,9 +19,27 @@ public class NewCommentController {
 
     private final NewCommentService newCommentService;
 
-    @GetMapping("/article/{category}/{articleId}/comments")
-    public Response<DetailPageCommentResponseDto> readArticleComments(@PathVariable Long articleId, @PathVariable Category category) {
+    @GetMapping("/article/{category}/{articleId}/comment")
+    public Response<DetailPageCommentResponseDto> read(@PathVariable Category category, @PathVariable Long articleId) {
         DetailPageCommentResponseDto dto = newCommentService.getDetailPageCommentList(articleId);
         return Response.success(dto);
+    }
+
+    @PostMapping("/comment")
+    public Response<NewCommentCreateResponseDto> create(@RequestBody NewCommentCreateRequestDto requestDto, @AuthenticationPrincipal AccountDto accountDto) {
+        NewCommentCreateResponseDto responseDto = newCommentService.createComment(requestDto, accountDto);
+        return Response.success(responseDto);
+    }
+
+    @PutMapping("/comment/{commentId}")
+    public Response update(@RequestBody NewCommentUpdateRequestDto requestDto, @PathVariable Long commentId, @AuthenticationPrincipal AccountDto accountDto) {
+        newCommentService.updateComment(requestDto, commentId, accountDto);
+        return Response.success();
+    }
+
+    @DeleteMapping("/comment/{commentId}")
+    public Response delete(@PathVariable Long commentId, @AuthenticationPrincipal AccountDto accountDto) {
+        newCommentService.deleteComment(commentId, accountDto);
+        return Response.success();
     }
 }
