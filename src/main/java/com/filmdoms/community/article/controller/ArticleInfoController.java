256307d804b8f4a044ec3588f765e29b36105846
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-11 16:41:19 +0900

    Feat : "카테고리에 안에 있는 태그들을 리턴하는 컨트롤러 메서드 작성"

--- src/main/java/com/filmdoms/community/article/controller/ArticleInfoController.java
+++ src/main/java/com/filmdoms/community/article/controller/ArticleInfoController.java
@@ -6,6 +6,7 @@ import com.filmdoms.community.article.data.constant.Tag;
 import com.filmdoms.community.article.data.dto.enuminfo.CategoryListDto;
 import com.filmdoms.community.article.data.dto.enuminfo.TagListDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class ArticleInfoController {
     public Response getCategoryInfo() {
         List<CategoryListDto> categoryListDtos = Arrays.stream(Category.values()).map(CategoryListDto::from).collect(Collectors.toList());
         return Response.success(categoryListDtos);
+    }
+
+    @GetMapping("/{category}")
+    public Response getTagInCategory(@PathVariable Category category) {
+        List<TagListDto> tagListDtos = Arrays.stream(Tag.values()).filter(tag -> tag.getCategory() == category).map(TagListDto::from).collect(Collectors.toList());
+        return Response.success(tagListDtos);
     }
 
     @GetMapping("/tag")
