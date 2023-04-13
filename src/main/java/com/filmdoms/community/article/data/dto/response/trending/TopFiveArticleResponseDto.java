68d26061d09cb9da8fca6c30cf1c66ef944d8051
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-13 19:12:29 +0900

    Feat : " 인기 게시물 반환 Dto Category 추가"

--- src/main/java/com/filmdoms/community/article/data/dto/response/trending/TopFiveArticleResponseDto.java
+++ src/main/java/com/filmdoms/community/article/data/dto/response/trending/TopFiveArticleResponseDto.java
@@ -1,6 +1,7 @@
 package com.filmdoms.community.article.data.dto.response.trending;
 
 import com.filmdoms.community.account.data.dto.response.SimpleAccountResponseDto;
+import com.filmdoms.community.article.data.constant.Category;
 import com.filmdoms.community.article.data.entity.Article;
 import lombok.Getter;
 
@@ -12,15 +13,18 @@ public class TopFiveArticleResponseDto {
     private SimpleAccountResponseDto author;
     private boolean containsImage;
 
+    private Category category;
 
-    public TopFiveArticleResponseDto(Long id, String title, SimpleAccountResponseDto author, boolean containsImage) {
+
+    public TopFiveArticleResponseDto(Long id, String title, SimpleAccountResponseDto author, boolean containsImage, Category category) {
         this.id = id;
         this.title = title;
+        this.category = category;
         this.containsImage = containsImage;
         this.author = author;
     }
 
     public static TopFiveArticleResponseDto from(Article article) {
-        return new TopFiveArticleResponseDto(article.getId(), article.getTitle(), SimpleAccountResponseDto.from(article.getAuthor()), article.isContainsImage());
+        return new TopFiveArticleResponseDto(article.getId(), article.getTitle(), SimpleAccountResponseDto.from(article.getAuthor()), article.isContainsImage(), article.getCategory());
     }
 }
