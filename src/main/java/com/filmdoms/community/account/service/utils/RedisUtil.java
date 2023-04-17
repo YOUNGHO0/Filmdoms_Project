7740138c0f52fe6adb97557686dc89bcb149bb81
Geon <gunner6603@snu.ac.kr>
2023-04-17 13:29:05 +0900

    Feat: RedisUtil 클래스 작성
    RedisTemplate이 노출되면 가독성이 떨어지고 테스트 코드 작성에 문제가 생기므로 RedisTemplate을 캡슐화하는 클래스를 따로 작성함

---
+++ src/main/java/com/filmdoms/community/account/service/utils/RedisUtil.java
@@ -0,0 +1,22 @@
+package com.filmdoms.community.account.service.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String getData(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void setDataAndExpirationInMillis(String key, String value, long millis) {
+        redisTemplate.opsForValue().set(key, value, Duration.ofMillis(millis));
+    }
+}
