fe5171f91251f52ecb76c0734f1cd35361ba36fb
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-18 15:59:47 +0900

    Feat : "공지사항 Repository 와 쿼리 구현"

---
+++ src/main/java/com/filmdoms/community/article/repository/AnnounceRepository.java
@@ -0,0 +1,26 @@
+package com.filmdoms.community.article.repository;
+
+import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.data.entity.extra.Announce;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AnnounceRepository extends JpaRepository<Announce, Long> {
+
+    @Query(value = "SELECT a from Announce a " +
+            "inner join fetch a.article " +
+            "inner join fetch a.article.author " +
+            "inner join a.article.author.profileImage"
+            , countQuery = "SELECT count(a) from Announce a")
+    Page<Announce> findAllAnnounceList(Pageable pageable);
+
+    @Query(value = "SELECT a from Announce a " +
+            "inner join fetch a.article " +
+            "inner join fetch a.article.author " +
+            "inner join a.article.author.profileImage where a.article.category =:categoryId"
+            , countQuery = "SELECT count(a) from Announce a inner join a.article where a.article.category =:categoryId")
+    Page<Announce> findAnnounceListByCategory(@Param("categoryId") Category category, Pageable pageable);
+}
