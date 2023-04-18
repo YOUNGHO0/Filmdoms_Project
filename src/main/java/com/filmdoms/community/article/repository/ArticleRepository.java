61281b10b6697f390f72586b1c089fd9f0cfdfe0
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-18 15:32:47 +0900

    Feat : "검색 관련 쿼리 추가"

--- src/main/java/com/filmdoms/community/article/repository/ArticleRepository.java
+++ src/main/java/com/filmdoms/community/article/repository/ArticleRepository.java
@@ -25,23 +25,39 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
             "WHERE a.id = :articleId")
     Optional<Article> findByIdWithAuthorProfileImageContent(@Param("articleId") Long articleId);
 
+    // 게시판 리스트 페이지 조회
     @Query(value = "SELECT a from Article a inner join fetch a.author join fetch a.author.profileImage where a.category =:categoryId"
             , countQuery = "SELECT count(a) from Article a where a.category =: categoryId")
     Page<Article> findArticlesByCategory(@Param("categoryId") Category category, Pageable pageable);
 
+    // 게시판 리스트 페이지에서 태그로 필터링
     @Query(value = "SELECT a from Article a inner join fetch a.author join fetch a.author.profileImage where a.category =:categoryId and a.tag =:tagId"
             , countQuery = "SELECT count(a) from Article a where a.category =: categoryId and a.tag =: tagId")
     Page<Article> findArticlesByCategoryAndTag(@Param("categoryId") Category category, @Param("tagId") Tag tag, Pageable pageable);
 
+    // (Recent) 최근 게시물 조회
     @Query(value = "SELECT a from Article a inner join fetch  a.author join fetch  a.author.profileImage"
             ,countQuery =  "SELECT count(a) FROM  Article a")
     Page<Article> getAllArticles(Pageable pageable);
 
+    // (Recent) 최근 게시물을 태그로 필터링
     @Query(value = "SELECT a from Article a inner join fetch  a.author join fetch  a.author.profileImage where a.tag =:tagId"
             ,countQuery =  "SELECT count(a) FROM  Article a where a.tag =:tagId")
     Page<Article> getAllArticlesByTag(@Param("tagId") Tag tag, Pageable pageable);
 
+    // 인기 게시글 조회
     @Query(value = "SELECT a from Article a inner join fetch a.author order by a.voteCount desc limit 5")
     List<Article> getTop5Articles();
+
+    // 제목 + 내용 으로 조회
+    @Query(value = "SELECT a from Article a inner join fetch a.author inner join fetch a.author.profileImage inner join fetch a.content " +
+            "where a.category =:categoryId and (a.title like %:keyword% or a.content.content like %:keyword%)"
+            , countQuery = "SELECT count(a) from Article a inner join a.content where a.category =:categoryId and (a.title like %:keyword% or a.content.content like %:keyword%) ")
+    Page<Article> findArticlesByKeyword(@Param("categoryId") Category category, @Param("keyword") String keyword, Pageable pageable);
+
+    // 글작성자로 조회
+    @Query(value = "SELECT a from Article a inner join fetch a.author join fetch a.author.profileImage where a.category =:categoryId and a.author.nickname =:nickname"
+            , countQuery = "SELECT count(a) from Article a inner join a.author where a.category =:categoryId and a.author.nickname =:nickname")
+    Page<Article> findArticlesByNickname(@Param("categoryId") Category category, @Param("nickname") String nickname, Pageable pageable);
 
 }
