b45fab9bdb029a578db7aa0401d0da1e80ea1770
YOUNGHO0 <31pqopq13@gmail.com>
2023-04-19 20:59:42 +0900

    Fix : "SearchMethod 를 받아 처리하게 변경"

--- src/main/java/com/filmdoms/community/article/controller/ArticleSearchController.java
+++ src/main/java/com/filmdoms/community/article/controller/ArticleSearchController.java
@@ -1,8 +1,8 @@
 package com.filmdoms.community.article.controller;
 
 import com.filmdoms.community.account.data.dto.response.Response;
-import com.filmdoms.community.account.exception.ErrorCode;
 import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.data.constant.SearchMethod;
 import com.filmdoms.community.article.data.dto.response.boardlist.ParentBoardListResponseDto;
 import com.filmdoms.community.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
@@ -17,31 +17,27 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/{category}/search")
+@RequestMapping("/api/v1/{category}/{searchMethod}")
 public class ArticleSearchController {
 
     private final ArticleService articleService;
 
     @GetMapping
-    public Response getSearchRequest(@PathVariable Category category, @RequestParam(required = false) String keyword, @RequestParam(required = false) String nickname,
+    public Response getSearchRequest(@PathVariable Category category, @PathVariable SearchMethod searchMethod, @RequestParam String keyword,
                                      @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         if (pageable.getPageSize() > 50)
             pageable = PageRequest.of(pageable.getPageNumber(), 24, Sort.by(Sort.Direction.DESC, "id"));
 
-        log.info("키워드 {}, 인포 {}", keyword, nickname);
-        if (keyword == null && nickname == null)
-            return Response.error(ErrorCode.NO_KEYWORD_FOUND.getMessage());
-        if (keyword != null && nickname != null)
-            return Response.error(ErrorCode.TOO_MANY_KEYWORD.getMessage());
+        Page<? extends ParentBoardListResponseDto> responseDtos;
+        switch (searchMethod) {
+            case TITLE_CONTENT:
+                responseDtos = articleService.findArticlesByKeyword(category, keyword, pageable);
+                return Response.success(responseDtos);
+            case NICKNAME:
+                responseDtos = articleService.findArticlesByNickname(category, keyword, pageable);
+                return Response.success(responseDtos);
+        }
 
-        if (keyword != null) {
-            Page<? extends ParentBoardListResponseDto> responseDtos = articleService.findArticlesByKeyword(category, keyword, pageable);
-            return Response.success(responseDtos);
-        }
-        if (nickname != null) {
-            Page<? extends ParentBoardListResponseDto> responseDtos = articleService.findArticlesByNickname(category, nickname, pageable);
-            return Response.success(responseDtos);
-        }
         return Response.error("정상적이지 않은 접근입니다");
     }
 }
