43ce793427a0e53e2aafe1215ebd11708e86b2ae
Geon <gunner6603@snu.ac.kr>
2023-04-22 13:43:54 +0900

    Feat: 테스트 코드 수정
    새로 생성한 TestAccountProvider를 사용해서 테스트 Account를 만들도록 테스트 코드 수정

--- src/test/java/com/filmdoms/community/comment/service/CommentServiceTest.java
+++ src/test/java/com/filmdoms/community/comment/service/CommentServiceTest.java
@@ -17,12 +17,15 @@ import com.filmdoms.community.comment.data.entity.CommentVote;
 import com.filmdoms.community.comment.data.entity.CommentVoteKey;
 import com.filmdoms.community.comment.repository.CommentRepository;
 import com.filmdoms.community.comment.repository.CommentVoteRepository;
+import com.filmdoms.community.testentityprovider.TestAccountProvider;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -53,9 +56,11 @@ class CommentServiceTest {
     @DisplayName("부모 댓글 생성이 정상적으로 이루어지는지 확인")
     void createParentComment() {
         //given
-        Account articleAuthor = createSampleAccount("articleAuthor");
+        Account articleAuthor = TestAccountProvider.get();
+        Account commentAuthor = TestAccountProvider.get();
+        accountRepository.saveAll(List.of(articleAuthor, commentAuthor));
+
         Article article = createSampleArticle(articleAuthor);
-        Account commentAuthor = createSampleAccount("commentAuthor");
         CommentCreateRequestDto requestDto = new CommentCreateRequestDto(article.getId(), null, "content", false);
 
         //when
@@ -73,10 +78,12 @@ class CommentServiceTest {
     @DisplayName("자식 댓글 생성이 정상적으로 이루어지는지 확인")
     void createChildComment() {
         //given
-        Account articleAuthor = createSampleAccount("articleAuthor");
+        Account articleAuthor = TestAccountProvider.get();
+        Account parentCommentAuthor = TestAccountProvider.get();
+        Account childCommentAuthor = TestAccountProvider.get();
+        accountRepository.saveAll(List.of(articleAuthor, parentCommentAuthor, childCommentAuthor));
+
         Article article = createSampleArticle(articleAuthor);
-        Account parentCommentAuthor = createSampleAccount("parentCommentAuthor");
-        Account childCommentAuthor = createSampleAccount("childCommentAuthor");
         Comment parentComment = createSampleComment(article, parentCommentAuthor, null);
         CommentCreateRequestDto requestDto = new CommentCreateRequestDto(article.getId(), parentComment.getId(), "content", false);
 
@@ -94,9 +101,11 @@ class CommentServiceTest {
     @Test
     @DisplayName("댓글 수정이 정상적으로 이루어지는지 확인")
     void updateComment() {
-        Account articleAuthor = createSampleAccount("articleAuthor");
+        Account articleAuthor = TestAccountProvider.get();
+        Account commentAuthor = TestAccountProvider.get();
+        accountRepository.saveAll(List.of(articleAuthor, commentAuthor));
+
         Article article = createSampleArticle(articleAuthor);
-        Account commentAuthor = createSampleAccount("commentAuthor");
         Comment comment = createSampleComment(article, commentAuthor, null);
         CommentUpdateRequestDto requestDto = new CommentUpdateRequestDto("updatedContent");
 
@@ -111,9 +120,11 @@ class CommentServiceTest {
     @Test
     @DisplayName("자식 댓글이 없는 부모 댓글 삭제가 정상적으로 이루어지는지 확인")
     void deleteComment() {
-        Account articleAuthor = createSampleAccount("articleAuthor");
+        Account articleAuthor = TestAccountProvider.get();
+        Account commentAuthor = TestAccountProvider.get();
+        accountRepository.saveAll(List.of(articleAuthor, commentAuthor));
+
         Article article = createSampleArticle(articleAuthor);
-        Account commentAuthor = createSampleAccount("commentAuthor");
         Comment comment = createSampleComment(article, commentAuthor, null);
 
         //when
@@ -126,9 +137,11 @@ class CommentServiceTest {
     @Test
     @DisplayName("추천 안한 댓글을 추천하면 추천수가 정상적으로 증가하는지 확인")
     void applyVoteComment() {
-        Account articleAuthor = createSampleAccount("articleAuthor");
+        Account articleAuthor = TestAccountProvider.get();
+        Account commentAuthor = TestAccountProvider.get();
+        accountRepository.saveAll(List.of(articleAuthor, commentAuthor));
+
         Article article = createSampleArticle(articleAuthor);
-        Account commentAuthor = createSampleAccount("commentAuthor");
         Comment comment = createSampleComment(article, commentAuthor, null);
 
         //when
@@ -143,9 +156,11 @@ class CommentServiceTest {
     @Test
     @DisplayName("추천한 댓글을 추천하면 추천수가 정상적으로 감소하는지 확인")
     void cancelVoteComment() {
-        Account articleAuthor = createSampleAccount("articleAuthor");
+        Account articleAuthor = TestAccountProvider.get();
+        Account commentAuthor = TestAccountProvider.get();
+        accountRepository.saveAll(List.of(articleAuthor, commentAuthor));
+
         Article article = createSampleArticle(articleAuthor);
-        Account commentAuthor = createSampleAccount("commentAuthor");
         Comment comment = createSampleComment(article, commentAuthor, null);
         createSampleVote(articleAuthor, comment);
 
@@ -156,13 +171,6 @@ class CommentServiceTest {
         assertThat(responseDto)
                 .hasFieldOrPropertyWithValue("isVoted", false)
                 .hasFieldOrPropertyWithValue("voteCount", 0);
-    }
-
-    private Account createSampleAccount(String username) {
-        Account account = Account.builder()
-                .username(username)
-                .build();
-        return accountRepository.save(account);
     }
 
     private Article createSampleArticle(Account author) {
