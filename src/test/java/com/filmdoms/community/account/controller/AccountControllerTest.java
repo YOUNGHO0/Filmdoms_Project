a09e452c2db0d60a7c76e50fdf1a4734c40690cc
TaemHam <tk4003950@gmail.com>
2023-04-24 14:35:53 +0900

    Test: "리프레시 토큰 관련 테스트 코드 작성"
    리프레시 토큰 기능 중 다음의 상황에 대해 테스트하는 코드를 작성했다.
    
    * 컨트롤러
    "액세스 토큰 재발급시, 올바른 리프레시 토큰을 헤더에 포함해 요청하면, 새로운 액세스 토큰을 발급한다."
    "로그 아웃시, 올바른 리프레시 토큰을 헤더에 포함해 요청하면, 성공 코드를 반환한다."
    
    * 서비스
    "토큰 재발급시, 올바른 리프레시 토큰을 입력하면, 새로운 액세스 토큰을 발급한다."
    "토큰 재발급시, 잘못된 리프레시 토큰을 입력하면, 예외를 반환한다."
    "로그 아웃시, 올바른 리프레시 토큰을 입력하면, 해당 리프레시 토큰을 무효화 시킨다."

--- src/test/java/com/filmdoms/community/account/controller/AccountControllerTest.java
+++ src/test/java/com/filmdoms/community/account/controller/AccountControllerTest.java
@@ -3,18 +3,26 @@ package com.filmdoms.community.account.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.filmdoms.community.account.config.SecurityConfig;
 import com.filmdoms.community.account.data.dto.request.LoginRequestDto;
+import com.filmdoms.community.account.data.dto.response.LoginResponseDto;
+import com.filmdoms.community.account.data.dto.response.RefreshAccessTokenResponseDto;
 import com.filmdoms.community.account.exception.ApplicationException;
 import com.filmdoms.community.account.exception.ErrorCode;
 import com.filmdoms.community.account.service.AccountService;
+import java.io.IOException;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -46,7 +54,11 @@ public class AccountControllerTest {
         // Given
         String email = "address@filmdoms.com";
         String password = "password";
-        when(accountService.login(email, password)).thenReturn("mockJwtString");
+        LoginResponseDto responseDto = LoginResponseDto.builder()
+                .accessToken("mockAccessToken")
+                .refreshToken("mockRefreshToken")
+                .build();
+        when(accountService.login(email, password)).thenReturn(responseDto);
 
         // When & Then
         mockMvc.perform(post("/api/v1/account/login")
@@ -55,7 +67,8 @@ public class AccountControllerTest {
                 ).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.[?(@.resultCode == 'SUCCESS')]").exists())
-                .andExpect(jsonPath("$..result[?(@..accessToken)]").exists());
+                .andExpect(jsonPath("$..result[?(@..accessToken)]").exists())
+                .andExpect(jsonPath("$..result[?(@..refreshToken)]").exists());
     }
 
     @Test
@@ -95,4 +108,39 @@ public class AccountControllerTest {
                 .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$.[?(@.resultCode == 'INVALID_PASSWORD')]").exists());
     }
+
+    @Test
+    @DisplayName("액세스 토큰 재발급시, 올바른 리프레시 토큰을 헤더에 포함해 요청하면, 새로운 액세스 토큰을 발급한다.")
+    void whenRefreshingAccessToken_thenReturnsNewAccessToken() throws Exception {
+        // Given
+        String refreshToken = "mockRefreshToken";
+        RefreshAccessTokenResponseDto responseDto = RefreshAccessTokenResponseDto.builder()
+                .accessToken("mockAccessToken")
+                .build();
+        when(accountService.refreshAccessToken(refreshToken)).thenReturn(responseDto);
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/account/refresh-token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + refreshToken)
+                ).andDo(print())
+                .andExpect(jsonPath("$.[?(@.resultCode == 'SUCCESS')]").exists())
+                .andExpect(jsonPath("$..result[?(@..accessToken)]").exists());
+    }
+
+    @Test
+    @DisplayName("로그 아웃시, 올바른 리프레시 토큰을 헤더에 포함해 요청하면, 성공 코드를 반환한다.")
+    void given_properRefreshToken_whenLoggingOut_thenReturnsNewAccessToken() throws Exception {
+
+        // Given
+        String refreshToken = "mockRefreshToken";
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/account/logout")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + refreshToken)
+                ).andDo(print())
+                .andExpect(jsonPath("$.[?(@.resultCode == 'SUCCESS')]").exists());
+    }
+
 }
