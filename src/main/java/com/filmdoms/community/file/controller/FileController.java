0364aeaba76a7f2b12aca33ed349890c4b15a3ba
Geon <gunner6603@snu.ac.kr>
2023-04-24 17:24:19 +0900

    Refactor: API URL 수정 및 응답 DTO 변경
    파일 업로드 요청 URL을 /api/v1/file로 수정함
    파일 업로드 완료 시 저장 파일명(uuidFileName)을 함께 응답하도록 변경
    변수명, 메서드명 수정
    
    resolves: #139

--- src/main/java/com/filmdoms/community/file/controller/FileController.java
+++ src/main/java/com/filmdoms/community/file/controller/FileController.java
@@ -2,7 +2,7 @@ package com.filmdoms.community.file.controller;
 
 import com.filmdoms.community.account.data.dto.response.Response;
 import com.filmdoms.community.file.service.AmazonS3UploadService;
-import com.filmdoms.community.file.data.dto.response.ImageUploadResponseDto;
+import com.filmdoms.community.file.data.dto.response.FileUploadResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +18,10 @@ public class FileController {
 
     private final AmazonS3UploadService amazonS3UploadService;
 
-    @PostMapping("/image")
-    public Response<ImageUploadResponseDto> uploadImages(
-            @RequestPart("image") List<MultipartFile> imageMultipartFiles) {
-        ImageUploadResponseDto responseDto = amazonS3UploadService.uploadAndSaveImages(imageMultipartFiles);
+    @PostMapping("/file")
+    public Response<FileUploadResponseDto> upload(
+            @RequestPart("file") List<MultipartFile> multipartFiles) {
+        FileUploadResponseDto responseDto = amazonS3UploadService.uploadAndSaveFiles(multipartFiles);
         return Response.success(responseDto);
     }
 
