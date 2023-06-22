6035ec958bbc81519bee4bff7cdfd013cdfc5f69
Geon <gunner6603@snu.ac.kr>
2023-06-22 14:27:56 +0900

    Refactor: board 패키지를 삭제

--- src/main/java/com/filmdoms/community/board/post/repository/PostCommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.filmdoms.community.board.post.repository;
-
-import com.filmdoms.community.board.post.data.entity.PostComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
-
-}
