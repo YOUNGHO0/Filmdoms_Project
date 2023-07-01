5c387223feae3cb88f01f320e1bea0bfd0394d36
Geon <gunner6603@snu.ac.kr>
2023-07-01 13:48:35 +0900

    Feat: 리포지토리 메소드 추가
    articleId를 이용해서 게시글 정보를 가져오는 메소드 추가

--- src/main/java/com/filmdoms/community/article/repository/FilmUniverseRepository.java
+++ src/main/java/com/filmdoms/community/article/repository/FilmUniverseRepository.java
@@ -11,17 +11,22 @@ import java.util.Optional;
 
 public interface FilmUniverseRepository extends JpaRepository<FilmUniverse, Long> {
 
-    @Query("SELECT n FROM FilmUniverse n " +
-            "LEFT JOIN FETCH n.article " +
-            "LEFT JOIN FETCH n.article.author " +
-            "LEFT JOIN FETCH n.mainImage")
+    @Query("SELECT f FROM FilmUniverse f " +
+            "LEFT JOIN FETCH f.article " +
+            "LEFT JOIN FETCH f.article.author " +
+            "LEFT JOIN FETCH f.mainImage")
     List<FilmUniverse> findAllWithArticleAuthorMainImage(Pageable pageable);
-    @Query("SELECT n FROM FilmUniverse n " +
-            "LEFT JOIN FETCH n.article " +
-            "LEFT JOIN FETCH n.article.author.profileImage " +
-            "LEFT JOIN FETCH n.article.content " +
-            "WHERE n.article.id = :articleId")
+    @Query("SELECT f FROM FilmUniverse f " +
+            "LEFT JOIN FETCH f.article " +
+            "LEFT JOIN FETCH f.article.author.profileImage " +
+            "LEFT JOIN FETCH f.article.content " +
+            "WHERE f.article.id = :articleId")
     Optional<FilmUniverse> findByArticleIdWithArticleAuthorProfileImageContent(@Param("articleId") Long articleId);
 
     Optional<FilmUniverse> findByArticleId(Long articleId);
+
+    @Query("SELECT f FROM FilmUniverse f " +
+            "LEFT JOIN FETCH f.article " +
+            "WHERE f.article.id = :articleId")
+    Optional<FilmUniverse> findByArticleIdWithArticle(@Param("articleId") Long articleId);
 }
