6ae4401be00f5af3a8b5b9552185b3ceeb2b6db2
Geon <gunner6603@snu.ac.kr>
2023-07-01 13:51:14 +0900

    Feat: 리포지토리 메소드 추가
    투표 정보를 가져오는 메소드 추가

--- src/main/java/com/filmdoms/community/vote/repository/VoteRepository.java
+++ src/main/java/com/filmdoms/community/vote/repository/VoteRepository.java
@@ -1,11 +1,22 @@
 package com.filmdoms.community.vote.repository;
 
+import com.filmdoms.community.article.data.entity.Article;
 import com.filmdoms.community.vote.data.entity.Vote;
 import com.filmdoms.community.vote.data.entity.VoteKey;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, VoteKey> {
 
     Optional<Vote> findByVoteKey(VoteKey voteKey);
+
+    @Modifying
+    @Query("DELETE FROM Vote v " +
+            "WHERE v.voteKey.article = :article")
+    void deleteByArticle(@Param("article") Article article);
 }
