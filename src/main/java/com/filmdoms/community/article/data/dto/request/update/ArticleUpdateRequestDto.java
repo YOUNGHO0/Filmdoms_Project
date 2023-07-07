30819529098d89ec22a51fb0a8f4f73e7a037ff9
Geon <gunner6603@snu.ac.kr>
2023-07-08 00:03:41 +0900

    Test: 게시글 업데이트에 대한 테스트 작성

--- src/main/java/com/filmdoms/community/article/data/dto/request/update/ArticleUpdateRequestDto.java
+++ src/main/java/com/filmdoms/community/article/data/dto/request/update/ArticleUpdateRequestDto.java
@@ -1,8 +1,16 @@
 package com.filmdoms.community.article.data.dto.request.update;
 
+import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.data.constant.Tag;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ArticleUpdateRequestDto extends ParentUpdateRequestDto {
+
+    public ArticleUpdateRequestDto(String title, Category category, Tag tag, String content, boolean containsImage) {
+        super(title, category, tag, content, containsImage);
+    }
 }
--- src/main/java/com/filmdoms/community/article/data/dto/request/update/FilmUniverseUpdateRequestDto.java
+++ src/main/java/com/filmdoms/community/article/data/dto/request/update/FilmUniverseUpdateRequestDto.java
@@ -1,8 +1,12 @@
 package com.filmdoms.community.article.data.dto.request.update;
 
+import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.data.constant.Tag;
 import com.filmdoms.community.article.data.entity.extra.FilmUniverse;
 import com.filmdoms.community.file.data.entity.File;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +21,13 @@ public class FilmUniverseUpdateRequestDto extends ParentUpdateRequestDto {
     private LocalDateTime endAt;
     @NotNull
     private Long mainImageId;
+
+    public FilmUniverseUpdateRequestDto(String title, Category category, Tag tag, String content, boolean containsImage, LocalDateTime startAt, LocalDateTime endAt, Long mainImageId) {
+        super(title, category, tag, content, containsImage);
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.mainImageId = mainImageId;
+    }
 
     public void updateEntity(FilmUniverse filmUniverse, File mainImage) {
         filmUniverse.update(mainImage, startAt, endAt);
