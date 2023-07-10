d28f2df5ddc04b78b2e1f2df7dbb0224ac092f06
YOUNGHO0 <31pqopq13@gmail.com>
2023-07-10 23:47:26 +0900

    Fix : content에 직접적으로 이미지 파일 태그가 들어가므로 백엔드에서 관리할 필요가 없으므로 List<FileResponseDto> images 삭제

--- src/main/java/com/filmdoms/community/article/data/dto/response/detail/ArticleDetailResponseDto.java
+++ src/main/java/com/filmdoms/community/article/data/dto/response/detail/ArticleDetailResponseDto.java
@@ -2,17 +2,13 @@ package com.filmdoms.community.article.data.dto.response.detail;
 
 import com.filmdoms.community.account.data.dto.response.DetailPageAccountResponseDto;
 import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.data.constant.PostStatus;
 import com.filmdoms.community.article.data.constant.Tag;
 import com.filmdoms.community.article.data.entity.Article;
-import com.filmdoms.community.article.data.constant.PostStatus;
-import com.filmdoms.community.file.data.dto.response.FileResponseDto;
-import com.filmdoms.community.file.data.entity.File;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import lombok.Getter;
 
-import java.util.Comparator;
-import java.util.List;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 /**
  * 영화, 비평 게시판의 게시글 상세 페이지 응답 DTO
@@ -32,9 +28,8 @@ public class ArticleDetailResponseDto {
     private long createdAt;
     private long updatedAt;
     private DetailPageAccountResponseDto author;
-    private List<FileResponseDto> images;
 
-    protected ArticleDetailResponseDto(Article article, List<File> images, boolean isVoted) {
+    protected ArticleDetailResponseDto(Article article, boolean isVoted) {
         this.id = article.getId();
         this.category = article.getCategory();
         this.tag = article.getTag();
@@ -47,10 +42,9 @@ public class ArticleDetailResponseDto {
         this.createdAt = ZonedDateTime.of(article.getDateCreated(), ZoneId.systemDefault()).toInstant().toEpochMilli();
         this.updatedAt = ZonedDateTime.of(article.getDateLastModified(), ZoneId.systemDefault()).toInstant().toEpochMilli();
         this.author = DetailPageAccountResponseDto.from(article.getAuthor());
-        this.images = images.stream().sorted(Comparator.comparing(File::getId)).map(FileResponseDto::from).toList(); //id로 정렬한 뒤 DTO 변환
     }
 
-    public static ArticleDetailResponseDto from(Article article, List<File> images, boolean isVoted) {
-        return new ArticleDetailResponseDto(article, images, isVoted);
+    public static ArticleDetailResponseDto from(Article article, boolean isVoted) {
+        return new ArticleDetailResponseDto(article, isVoted);
     }
 }
