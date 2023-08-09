331de350b2035ce09887cc4c82f20cc99d1ee1cc
YOUNGHO0 <31pqopq13@gmail.com>
2023-08-09 17:28:32 +0900

    Feat : 각 카테고리의 게시글들을 전부 삭제하는 AdminService 생성 * Movie 게시글의 경우에는 모든 카테고리들이 같이 들어있으므로, filter로 영화 게시글만 필터링 하였음

---
+++ src/main/java/com/filmdoms/community/article/service/AdminService.java
@@ -0,0 +1,38 @@
+package com.filmdoms.community.article.service;
+
+import com.filmdoms.community.account.data.dto.AccountDto;
+import com.filmdoms.community.account.data.dto.response.Response;
+import com.filmdoms.community.account.repository.AccountRepository;
+import com.filmdoms.community.article.data.constant.Category;
+import com.filmdoms.community.article.repository.ArticleRepository;
+import com.filmdoms.community.article.repository.CriticRepository;
+import com.filmdoms.community.article.repository.FilmUniverseRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AdminService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
+    private final CriticRepository criticRepository;
+    private final AccountRepository accountRepository;
+    private final FilmUniverseRepository filmUniverseRepository;
+
+    public Response deleteAllArticlesService(Category category, AccountDto accountDto) {
+
+        switch (category) {
+            case MOVIE ->
+                    articleRepository.findAll().stream().filter(article -> article.getCategory() == Category.MOVIE).forEach(movie -> articleService.deleteArticle(Category.MOVIE, movie.getId(), accountDto));
+            case CRITIC ->
+                    criticRepository.findAll().stream().forEach(critic -> articleService.deleteArticle(Category.CRITIC, critic.getArticle().getId(), accountDto));
+            case FILM_UNIVERSE ->
+                    filmUniverseRepository.findAll().stream().forEach(critic -> articleService.deleteArticle(Category.FILM_UNIVERSE, critic.getArticle().getId(), accountDto));
+        }
+
+        return Response.success();
+    }
+}
