8f61496ee829a06ab63808d481389c3ac7fd7e52
YOUNGHO0 <31pqopq13@gmail.com>
2023-08-12 18:41:19 +0900

    Fix : 메인 페이지로 나가는 Dto 수정 * File 의존성이 없기 때문에 FileResponseDto를 사용하지 않고 그대로 String 으로 Dto를 내보냄

--- src/main/java/com/filmdoms/community/article/data/dto/response/mainpage/CriticMainPageResponseDto.java
+++ src/main/java/com/filmdoms/community/article/data/dto/response/mainpage/CriticMainPageResponseDto.java
@@ -2,26 +2,24 @@ package com.filmdoms.community.article.data.dto.response.mainpage;
 
 import com.filmdoms.community.account.data.dto.response.SimpleAccountResponseDto;
 import com.filmdoms.community.article.data.entity.extra.Critic;
-import com.filmdoms.community.file.data.dto.response.FileResponseDto;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import lombok.Getter;
 
-import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 /**
  * 메인 페이지의 비평 카테고리, recent 게시글 응답 DTO
  */
 @Getter
 public class CriticMainPageResponseDto extends ParentMainPageResponseDto {
-    private FileResponseDto mainImage;
+    private String mainImage;
     private SimpleAccountResponseDto author;
     private String description;
     private long createdAt;
 
     private CriticMainPageResponseDto(Critic critic) {
         super(critic.getArticle());
-        this.mainImage = FileResponseDto.from(critic.getMainImage());
+        this.mainImage = critic.getMainImage();
         this.author = SimpleAccountResponseDto.from(critic.getArticle().getAuthor());
         this.description = critic.getArticle().getContent().getContent();
         this.createdAt = ZonedDateTime.of(critic.getArticle().getDateCreated(), ZoneId.systemDefault()).toInstant().toEpochMilli();
