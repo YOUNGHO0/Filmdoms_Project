b9a9d25f8d60842078b8fa07a335f04ef39f7f7a
YOUNGHO0 <31pqopq13@gmail.com>
2023-08-16 17:07:16 +0900

    Feat : 게시글 에디터의 내용 요청을 받는 ArticleInfoController 생성

--- src/main/java/com/filmdoms/community/article/controller/ArticleInfoController.java
+++ src/main/java/com/filmdoms/community/article/controller/ArticleInfoController.java
@@ -3,8 +3,12 @@ package com.filmdoms.community.article.controller;
 import com.filmdoms.community.account.data.dto.response.Response;
 import com.filmdoms.community.article.data.constant.Category;
 import com.filmdoms.community.article.data.constant.Tag;
+import com.filmdoms.community.article.data.dto.response.editor.ArticleEditorResponseDto;
 import com.filmdoms.community.article.data.dto.response.enuminfo.CategoryListDto;
 import com.filmdoms.community.article.data.dto.response.enuminfo.TagListDto;
+import com.filmdoms.community.article.service.ArticleEditorService;
+import com.filmdoms.community.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +19,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class ArticleInfoController {
+
+    private final ArticleEditorService articleEditorService;
 
     @GetMapping("/category")
     public Response getCategoryInfo() {
@@ -35,4 +42,23 @@ public class ArticleInfoController {
         List<TagListDto> tagListDtos = Arrays.stream(Tag.values()).map(TagListDto::from).collect(Collectors.toList());
         return Response.success(tagListDtos);
     }
+
+    @GetMapping("/editor/{category}")
+    public Response getEditorNotice(@PathVariable Category category) {
+
+        ArticleEditorResponseDto dto = null;
+
+        switch (category) {
+            case CRITIC -> dto = articleEditorService.getCriticNotice();
+            case MOVIE -> dto = articleEditorService.getMovieNotice();
+            case FILM_UNIVERSE -> dto = articleEditorService.getFilmUniverseNotice();
+        }
+
+        if (dto == null)
+            return Response.error(ErrorCode.CATEGORY_DOES_NOT_MATCH.getMessage());
+        else
+            return Response.success(dto);
+
+    }
+
 }
